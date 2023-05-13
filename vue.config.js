20a7a4a57ca3261d259bd6d46fb25da165a89cdd
roelbattad <battad09@gmail.com>
2023-05-13 17:22:59 +0800

    modify vue config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
   transpileDependencies: true,
   configureWebpack: {
     plugins: [new MiniCssExtractPlugin()],
